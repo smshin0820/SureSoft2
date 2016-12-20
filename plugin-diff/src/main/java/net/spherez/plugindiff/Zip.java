7c015a9dd5be49708ce3eefc73c83f48c3cabd87
Administrator <Administrator@MSDN-SPECIAL>
2016-12-20 15:02:08 +0900

    불필요한 주석 및 출력 확인한 것 지움.

--- plugin-diff/src/main/java/net/spherez/plugindiff/Zip.java
+++ plugin-diff/src/main/java/net/spherez/plugindiff/Zip.java
@@ -72,12 +72,10 @@ public class Zip {
 		byte[] buf = new byte[4096];
 
 		try {
-			System.out.println("zipFileName : " + zipFileName);
 			ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFileName));
 			
 			boolean firstFlag = true;
 			for (String file : fileList) {
-				System.out.println("before : " + file);
 				FileInputStream in = new FileInputStream(file);
 				if(firstFlag){
 					firstFlag = false;
@@ -86,7 +84,6 @@ public class Zip {
 				}else{
 					file = file.substring(plugin.getRootPathInput().length());
 				}
-				System.out.println("after : " + file);
 				
 				ZipEntry ze = new ZipEntry(file);
 				out.putNextEntry(ze);
