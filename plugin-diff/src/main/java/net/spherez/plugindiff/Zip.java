274b19579293d7a769d3b4543ed293f2054208d5
Administrator <Administrator@MSDN-SPECIAL>
2016-12-20 13:56:55 +0900

    [변경전]패치가 필요한 파일목록들만 모아서 압축. [변경후]패치가 필요한 파일들을 상위 디렉토리까지 포함해서 압축.

--- plugin-diff/src/main/java/net/spherez/plugindiff/Zip.java
+++ plugin-diff/src/main/java/net/spherez/plugindiff/Zip.java
@@ -2,13 +2,14 @@ package net.spherez.plugindiff;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -30,7 +31,13 @@ public class Zip {
 		// ArrayList에 담겨있는 애들만 압축하기
 		// 압축이 필요한 파일만 fileList에 넣기
 		ArrayList<String> fileList = new ArrayList<>();
-		String zipFileName = System.getProperty("user.dir")+"\\diffLogs\\patch.zip";
+		
+		// 현재 날짜 구하기
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		String date = sdf.format(cal.getTime());
+		
+		String zipFileName = System.getProperty("user.dir")+"\\diffLogs\\patch" + date + ".zip";
 		
 		FileReader fr = null;
 		BufferedReader br = null;
@@ -65,14 +72,23 @@ public class Zip {
 		byte[] buf = new byte[4096];
 
 		try {
+			System.out.println("zipFileName : " + zipFileName);
 			ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFileName));
 			
+			boolean firstFlag = true;
 			for (String file : fileList) {
+				System.out.println("before : " + file);
 				FileInputStream in = new FileInputStream(file);
-				Path p = Paths.get(file);
-				String fileName = p.getFileName().toString();
-
-				ZipEntry ze = new ZipEntry(fileName);
+				if(firstFlag){
+					firstFlag = false;
+					Path p = Paths.get(file);
+					file = p.getFileName().toString();
+				}else{
+					file = file.substring(plugin.getRootPathInput().length());
+				}
+				System.out.println("after : " + file);
+				
+				ZipEntry ze = new ZipEntry(file);
 				out.putNextEntry(ze);
 
 				int len = 0;
@@ -89,5 +105,7 @@ public class Zip {
 		}
 
 	} // archive end
+	
+	
 
 }
